0d118c331b7401e58ed9aa8daa669fb8877fea64
Shravanikuldharan <shravanikuldharan05@gmail.com>
2025-08-11 14:48:18 +0530

    Changes in login page

--- src/views/login/Login.jsx
+++ src/views/login/Login.jsx
@@ -4,7 +4,7 @@ import { FaGoogle, FaFacebook, FaMicrosoft } from 'react-icons/fa';
 import loginImage from '../../assets/Login.png';
 import Navbar from '../../components/Navbar.jsx';
 import { Toaster, toast } from 'react-hot-toast';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import Footer from '../../components/Footer.jsx';
 import Button from '../../components/Button.jsx';
 
@@ -88,9 +88,10 @@ function Login() {
 
             <p className="mt-4 text-center text-sm text-gray-600">
               Don't have an account?{' '}
-              <a href="/register" className="text-[#159ab7] hover:text-[#ff0000] font-medium">
-                Register here
-              </a>
+              <Link to="/register" className="text-[#159ab7] hover:text-[#ff0000] font-medium">
+              Register here
+            </Link>
+
             </p>
 
             <div className="mt-6 flex justify-center gap-6 text-2xl">
