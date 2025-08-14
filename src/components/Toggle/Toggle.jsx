56854c2f857aba49587f37ac4eb9ee7e5e433c0b
Prayag-waje <prayagwaje21@gmail.com>
2025-08-14 17:23:32 +0530

    setting page updated

---
+++ src/components/Toggle/Toggle.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Toggle.css"
+
+function Toggle({handlechange, ischecked }) {
+  return (
+    <div className='toggle-container'>
+      
+    </div>
+  )
+}
+
+export default Toggle
