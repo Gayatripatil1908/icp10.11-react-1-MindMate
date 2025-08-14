b4c0fa884960941b4bce9bac7cff3c215211f25c
Prayag-waje <prayagwaje21@gmail.com>
2025-08-14 17:24:10 +0530

    toggle component created

--- src/components/Toggle/Toggle.jsx
+++ src/components/Toggle/Toggle.jsx
@@ -4,7 +4,14 @@ import "./Toggle.css"
 function Toggle({handlechange, ischecked }) {
   return (
     <div className='toggle-container'>
-      
+      <input 
+      className='toggle' 
+      type='checkbox'
+      id='check'
+      onChange={handlechange}
+      checked = {ischecked}
+      />
+      <label htmlFor="check"></label>
     </div>
   )
 }
